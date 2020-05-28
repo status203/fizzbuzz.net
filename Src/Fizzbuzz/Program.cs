b33b2aa0a15cec9176dd319a27d592d82a5bafc9
Adam Gibbs <git@status203.com>
2020-05-28 15:09:30 +0100

    Initial genereated solution

---
+++ Src/Fizzbuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
